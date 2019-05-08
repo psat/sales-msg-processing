7379cc4679ea2c64c235071278d7877b6ce0723a
Sérgio Teixeira <psat@users.noreply.github.com>
2019-05-08 18:18:27 +0100

    Create initial project structure
    Add app's main entry point
    Add pom.xml file

---
+++ src/main/java/com/psat/SalesMsgProcessingApp.java
@@ -0,0 +1,13 @@
+package com.psat;
+
+import java.util.logging.Logger;
+
+public class SalesMsgProcessingApp {
+
+  private static final Logger LOGGER = Logger.getLogger(SalesMsgProcessingApp.class.getSimpleName());
+
+  public static void main(String[] varargs) {
+    LOGGER.warning(() -> "Nothing happening");
+  }
+
+}
