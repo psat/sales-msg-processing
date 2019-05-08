0e2f80babd9a917339641c58535a28735fd4a953
Sérgio Teixeira <psat@users.noreply.github.com>
2019-05-08 18:22:22 +0100

    Add class to process messages
    New class to process messages
    Add unit test - failing test

---
+++ src/main/java/com/psat/MsgProcessingService.java
@@ -0,0 +1,9 @@
+package com.psat;
+
+public class MsgProcessingService {
+
+    public void process() {
+
+    }
+
+}
---
+++ src/test/java/com/psat/MsgProcessingServiceTest.java
@@ -0,0 +1,14 @@
+package com.psat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class MsgProcessingServiceTest {
+
+    @Test
+    public void givenMessage_whenToBeProcessed_thenMessageGetsRecorded() {
+        fail("nothing recorded");
+    }
+
+}
